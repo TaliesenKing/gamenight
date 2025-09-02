74e3b236c183112b24d43c2c15654be4efd645c9
taliesenking <taliesenking@yahoo.com>
2025-09-01 20:07:52 -0700

    Fix circular imports and schema configuration in models

--- app/models/attendee.py
+++ app/models/attendee.py
@@ -1,10 +1,13 @@
-from app.models.db import db
+from app.models.db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Attendee(db.Model):
     __tablename__ = 'attendees'
 
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
-    game_night_id = db.Column(db.Integer, db.ForeignKey('game_nights.id'), primary_key=True)
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True)
+    game_night_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('game_nights.id')), primary_key=True)
     
     
     user = db.relationship('User', back_populates='attendances')
--- app/models/game_night.py
+++ app/models/game_night.py
@@ -1,6 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
-from .attendee import Attendee
 
 class GameNight(db.Model):
     __tablename__ = 'game_nights'
